b930542b92fa0f92b1d1589b897c1930c31b377c
bandar2014505 <89901403+bandar2014505@users.noreply.github.com>
2022-04-11 22:50:14 +0300

    Delete Source/projweb directory

--- Source/projweb/server.php
+++
@@ -1,9 +0,0 @@
-<?php
-$username = "root";
-$password = "";
-$datebase = new 
-PDO("mysql:host=localhost;dbname=ajrnidb;charset=utf8;",$username,$password,);
-if($datebase){
-   echo 'تم الاتصال بقاعدة البيانات' ;
-}
-?>
