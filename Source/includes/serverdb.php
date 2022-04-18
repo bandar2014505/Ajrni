cf21d46b3e5d8f765d19601292e877ee149ba5a1
Mohannd3050 <98720111+Mohannd3050@users.noreply.github.com>
2022-04-19 01:49:26 +0300

    Update serverdb.php

--- Source/includes/serverdb.php
+++ Source/includes/serverdb.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $passworddb = "";
-$database = "sql";
+$database = "ajrnidb";
 
 $conn = new mysqli($servername, $username, $passworddb,$database);
 date_default_timezone_set("Asia/Riyadh");
